970f42202241aad8eb457c76ee2801167681a42b
Gustavopvs1 <polen451@gmail.com>
2023-12-01 23:22:55 -0600

    categorias y restaurantes cercanos

--- src/components/navigation/StackNavigation/LocalesScreen.js
+++ src/components/navigation/StackNavigation/LocalesScreen.js
@@ -1,21 +1,78 @@
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { TouchableOpacity, Image, View, Text, StyleSheet, ScrollView } from 'react-native';
+import axios from 'axios';
 
-const LocalesScreen = () => {
+const LocalesScreen = ({ route }) => {
+    const [queretaroLocation, setQueretaroLocation] = useState({
+        latitude: 20.5881, 
+        longitude: -100.3899, 
+    });
+    const [localPlaces, setLocalPlaces] = useState([]);
+
+    useEffect(() => {
+        getNearbyLocalPlaces(queretaroLocation);
+    }, []);
+
+    const getNearbyLocalPlaces = async (location) => {
+        try {
+            const apiKey = 'AIzaSyAxT9y1e41YVMLKKluEKxjS2ctQ1PzlAEw';
+            const radius = 5000; 
+            const type = 'restaurant';
+            const keywords = ['taqueria', 'gorditas', 'tortas', 'garnachas'];
+
+            // Crea una promesa para realizar varias búsquedas con diferentes palabras clave
+            const requests = keywords.map(async (keyword) => {
+                const response = await axios.get(
+                    `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${location.latitude},${location.longitude}&radius=${radius}&type=${type}&keyword=${keyword}&key=${apiKey}`
+                );
+                return response.data.results;
+            });
+
+            // Espera a que todas las búsquedas estén completas
+            const results = await Promise.all(requests);
+
+            // Combina los resultados en una sola lista
+            const combinedResults = results.reduce((acc, result) => acc.concat(result), []);
+
+            // Elimina duplicados
+            const uniqueResults = Array.from(new Set(combinedResults.map((place) => place.place_id))).map(
+                (placeId) => combinedResults.find((place) => place.place_id === placeId)
+            );
+
+            setLocalPlaces(uniqueResults);
+        } catch (error) {
+            console.error('Error al obtener lugares locales:', error);
+        }
+    };
+
     return (
-        <View style={styles.container}>
-            <Text>Screen de Locales</Text>
-        </View>
+        <ScrollView contentContainerStyle={{ padding: 16 }}>
+
+            {localPlaces.map((place) => (
+          <TouchableOpacity
+          key={place.place_id}
+          onPress={() => {
+            // Agrega la lógica para navegar a la pantalla de detalles del restaurante
+            // Pasando la información del restaurante como parámetro, por ejemplo:
+            navigation.navigate('DetallesRestaurante', { place });
+          }}
+        >
+          <View style={{ marginBottom: 16 }}>
+            <Image
+              source={{ uri: place.icon }}
+              style={{ width: 50, height: 50, marginRight: 10 }}
+            />
+            <View>
+              <Text style={{ fontSize: 18, fontWeight: 'bold' }}>
+                {place.name}
+              </Text>
+              <Text>{place.vicinity}</Text>
+            </View>
+          </View>
+        </TouchableOpacity>
+            ))}
+        </ScrollView>
     );
 };
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: '#E0E0E0', // Fondo de color #E0E0E0
-    },
-});
 
 export default LocalesScreen;
--- src/components/navigation/StackNavigation/StackHome.js
+++ src/components/navigation/StackNavigation/StackHome.js
@@ -1,200 +1,145 @@
 import React, { useEffect, useState } from 'react';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import {
-    TouchableOpacity,
-    Image,
-    View,
-    Text,
-    StyleSheet,
-    ScrollView,
-} from 'react-native';
-import { List, Card } from 'react-native-paper';
-import { getPlaces, getPlaceDetails } from '../../../api/ApiGoogle';
-import PlaceDetailComponent from './PlaceDetailComponent';
+import { TouchableOpacity, Image, View, Text, ScrollView } from 'react-native';
+import StackAccount from '../StackNavigation/StackAccount';
+import axios from 'axios';
+import * as Location from 'expo-location';
 
 const StackHome = ({ navigation }) => {
-    const Stack = createNativeStackNavigator();
+  const Stack = createNativeStackNavigator();
 
-    return (
-        <Stack.Navigator
-            screenOptions={{
-                headerRight: () => (
-                    <TouchableOpacity
-                        onPress={() => navigation.navigate('StackAccount')}
-                    >
-                        <View
-                            style={{
-                                backgroundColor: '#24EF2C', // Fondo blanco
-                                borderRadius: 23, // Borde redondeado
-                                padding: 2, // Espaciado interno
-                                marginRight: 3,
-                            }}
-                        >
-                            <Image
-                                source={require('../../../assets/person1.jpeg')}
-                                style={{
-                                    width: 45,
-                                    height: 45,
-                                    borderRadius: 20,
-                                    overflow: 'hidden', // Oculta el contenido que se desborda
-                                }}
-                            />
-                        </View>
-                    </TouchableOpacity>
-                ),
-                headerStyle: {
-                    backgroundColor: '#E0E0E0', // Fondo blanco de la barra de navegación
-                },
-                headerShadowVisible: false, // Quitar la sombra
-            }}
-        >
-            <Stack.Screen
-                name='Home'
-                component={HomeScreen}
-                options={{
-                    headerTitle: '',
+  return (
+    <Stack.Navigator
+      screenOptions={{
+        headerRight: () => (
+          <TouchableOpacity
+            onPress={() => navigation.navigate('StackAccount')}
+          >
+            <View
+              style={{
+                backgroundColor: '#24EF2C', // Fondo blanco
+                borderRadius: 23, // Borde redondeado
+                padding: 2, // Espaciado interno
+                marginRight: 3,
+              }}
+            >
+              <Image
+                source={require('../../../assets/person1.jpeg')}
+                style={{
+                  width: 45,
+                  height: 45,
+                  borderRadius: 20,
+                  overflow: 'hidden', // Oculta el contenido que se desborda
                 }}
-            />
-            <Stack.Screen
-                name='PlaceDetail'
-                component={PlaceDetailComponent}
-                options={({ route }) => ({
-                    title: 'Detalles',
-                })}
-            />
-        </Stack.Navigator>
-    );
+              />
+            </View>
+          </TouchableOpacity>
+        ),
+        headerStyle: {
+          backgroundColor: '#E0E0E0', // Fondo blanco de la barra de navegación
+        },
+        headerShadowVisible: false, // Quitar la sombra
+      }}
+    >
+      <Stack.Screen
+        name='Home'
+        component={HomeScreen}
+        options={{
+          headerTitle: '', // Esto quita el texto "Home"
+        }}
+      />
+    </Stack.Navigator>
+  );
 };
 
 const HomeScreen = ({ navigation }) => {
-    const [places, setPlaces] = useState([]);
+  const [restaurants, setRestaurants] = useState([]);
+  const [currentLocation, setCurrentLocation] = useState(null);
 
-    useEffect(() => {
-        const location = '20.65367776367199, -100.40503298430045';
-        const radius = 1000;
-        const types = 'restaurant';
-        const query = 'food';
-        const region = 'mx:qro';
-        const maxResults = 10;
+  useEffect(() => {
+    // Obtén la ubicación actual al cargar la pantalla
+    getCurrentLocation();
+  }, []);
 
-        const fetchData = async () => {
-            try {
-                const response = await getPlaces(
-                    location,
-                    radius,
-                    types,
-                    query,
-                    region,
-                    maxResults
-                );
+  const getCurrentLocation = async () => {
+    try {
+      // Solicita permisos de ubicación
+      const { status } = await Location.requestForegroundPermissionsAsync();
 
-                const detailedPlaces = await Promise.all(
-                    response.results.map(async (place, index) => {
-                        const details = await getPlaceDetails(place.place_id);
-                        return {
-                            id: `${place.place_id}_${index}`,
-                            name: place.name,
-                            detail: details.formatted_address,
-                            reviewCount: details.user_ratings_total,
-                        };
-                    })
-                );
+      if (status !== 'granted') {
+        console.warn('Permiso de ubicación denegado');
+        return;
+      }
 
-                setPlaces(detailedPlaces);
-            } catch (error) {
-                console.error(error);
-            }
-        };
+      // Obtiene la ubicación actual
+      const location = await Location.getCurrentPositionAsync({});
+      setCurrentLocation(location.coords);
 
-        fetchData();
-    }, []);
+      // Si tienes la ubicación actual, realiza la solicitud de lugares cercanos
+      if (location) {
+        getNearbyRestaurants(location.coords);
+      }
+    } catch (error) {
+      console.error('Error al obtener la ubicación actual:', error);
+    }
+  };
 
-    return (
-        <ScrollView contentContainerStyle={styles.container}>
-            {places.map((place) => (
-                <TouchableOpacity
-                    key={place.id}
-                    onPress={() =>
-                        navigation.navigate('PlaceDetail', { place })
-                    }
-                >
-                    <Card key={place.id} style={styles.card}>
-                        <Card.Content style={styles.cardContent}>
-                            <View style={styles.iconContainer}>
-                                <List.Icon
-                                    icon='hamburger'
-                                    color='#000'
-                                    style={styles.hamburgerIcon}
-                                />
-                            </View>
-                            <View style={styles.textContainer}>
-                                <Text style={styles.title}>{place.name}</Text>
-                                <Text style={styles.detail}>
-                                    Reseñas: {place.reviewCount} | Dirección:{' '}
-                                    {place.detail}
-                                </Text>
-                            </View>
-                            <List.Icon
-                                icon='chevron-right'
-                                color='#000'
-                                style={styles.rightArrowIcon}
-                            />
-                        </Card.Content>
-                    </Card>
-                </TouchableOpacity>
-            ))}
-        </ScrollView>
-    );
-};
+  const getNearbyRestaurants = async (location) => {
+    try {
+      const apiKey = 'AIzaSyAxT9y1e41YVMLKKluEKxjS2ctQ1PzlAEw';
+      const radius = 5000; // Radio de búsqueda en metros
+      const type = 'restaurant'; 
 
-const styles = StyleSheet.create({
-    container: {
-        backgroundColor: '#E0E0E0',
-        alignItems: 'center',
-        padding: 14,
-    },
-    card: {
-        marginBottom: 15,
-        height: 160,
-        width: 352,
-    },
-    cardContent: {
-        flexDirection: 'row',
-        alignItems: 'center',
-    },
-    iconContainer: {
-        backgroundColor: '#DAE4F1',
-        borderTopLeftRadius: 10,
-        borderBottomLeftRadius: 10,
-        width: 80,
-        height: 160,
-        justifyContent: 'center',
-        marginRight: 5,
-        marginTop: -16,
-        marginLeft: -20,
-        paddingVertical: 5,
-    },
-    hamburgerIcon: {},
-    textContainer: {
+      const response = await axios.get(
+        `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${location.latitude},${location.longitude}&radius=${radius}&type=${type}&key=${apiKey}`
+      );
+
+      if (response.data && response.data.results) {
+        setRestaurants(response.data.results);
+      }
+    } catch (error) {
+      console.error('Error al obtener restaurantes:', error);
+    }
+  };
+
+  return (
+    <View
+      style={{
         flex: 1,
-        marginTop: -30,
-    },
-    title: {
-        color: '#000',
-        fontWeight: 'bold',
-        fontSize: 16, // Ajusta según sea necesario
-    },
-    description: {
-        color: '#000',
-        fontSize: 14, // Ajusta según sea necesario
-    },
-    detail: {
-        color: '#000',
-        fontSize: 12, // Ajusta según sea necesario
-    },
-    rightArrowIcon: {
-        marginLeft: 'auto',
-    },
-});
+        backgroundColor: '#E0E0E0', // Color de fondo principal
+      }}
+    >
+      <ScrollView contentContainerStyle={{ padding: 16 }}>
+        <Text style={{ fontSize: 20, fontWeight: 'bold', marginBottom: 16 }}>
+          Restaurantes Cercanos
+        </Text>
+
+        {restaurants.map((restaurant) => (
+          <TouchableOpacity
+            key={restaurant.place_id}
+            onPress={() => {
+              // Agrega la lógica para navegar a la pantalla de detalles del restaurante
+              // Pasando la información del restaurante como parámetro, por ejemplo:
+              navigation.navigate('DetallesRestaurante', { restaurant });
+            }}
+          >
+            <View style={{ marginBottom: 16 }}>
+              <Image
+                source={{ uri: restaurant.icon }}
+                style={{ width: 50, height: 50, marginRight: 10 }}
+              />
+              <View>
+                <Text style={{ fontSize: 18, fontWeight: 'bold' }}>
+                  {restaurant.name}
+                </Text>
+                <Text>{restaurant.vicinity}</Text>
+              </View>
+            </View>
+          </TouchableOpacity>
+        ))}
+      </ScrollView>
+    </View>
+  );
+};
 
 export default StackHome;
