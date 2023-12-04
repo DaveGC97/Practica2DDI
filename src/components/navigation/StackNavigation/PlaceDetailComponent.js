4ba609675310b87f2caeabce5863671bb1833735
DaveGC97 <2020371123@uteq.edu.mx>
2023-12-03 19:01:05 -0600

    comentarios

--- src/components/navigation/StackNavigation/PlaceDetailComponent.js
+++ src/components/navigation/StackNavigation/PlaceDetailComponent.js
@@ -1,5 +1,12 @@
 import React, { useState, useEffect } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import {
+    View,
+    Text,
+    StyleSheet,
+    TextInput,
+    Button,
+    ScrollView,
+} from 'react-native';
 import { IconButton } from 'react-native-paper';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import StarRating from './StarRating';
@@ -7,6 +14,8 @@ import StarRating from './StarRating';
 const PlaceDetailComponent = ({ route, navigation }) => {
     const place = route.params?.place || {};
     const [rating, setRating] = useState(0);
+    const [comments, setComments] = useState([]);
+    const [newComment, setNewComment] = useState('');
 
     const getRatingFromStorage = async () => {
         try {
@@ -34,6 +43,49 @@ const PlaceDetailComponent = ({ route, navigation }) => {
         }
     };
 
+    const getCommentsFromStorage = async () => {
+        try {
+            const storedComments = await AsyncStorage.getItem(
+                `comments_${place.id}`
+            );
+
+            if (storedComments !== null) {
+                setComments(JSON.parse(storedComments));
+            }
+        } catch (error) {
+            console.error('Error retrieving comments from storage:', error);
+        }
+    };
+
+    const saveCommentsToStorage = async (comments) => {
+        try {
+            await AsyncStorage.setItem(
+                `comments_${place.id}`,
+                JSON.stringify(comments)
+            );
+        } catch (error) {
+            console.error('Error saving comments to storage:', error);
+        }
+    };
+
+    const handleDeleteComment = (index) => {
+        const updatedComments = [...comments];
+        updatedComments.splice(index, 1);
+        setComments(updatedComments);
+        saveCommentsToStorage(updatedComments);
+    };
+
+    const handleCommentChange = (text) => {
+        setNewComment(text);
+    };
+
+    const handleAddComment = () => {
+        const updatedComments = [...comments, newComment];
+        setComments(updatedComments);
+        saveCommentsToStorage(updatedComments);
+        setNewComment('');
+    };
+
     const handleRatingChange = (newRating) => {
         setRating(newRating);
         saveRatingToStorage(newRating); // Guardar el nuevo valor al cambiar la calificación
@@ -47,11 +99,12 @@ const PlaceDetailComponent = ({ route, navigation }) => {
     useEffect(() => {
         // Al entrar al detalle, actualizamos la calificación
         getRatingFromStorage();
+        getCommentsFromStorage();
         // También puedes hacer otras cosas al entrar al detalle si es necesario
     }, [navigation, place.id]); // Agrega place.id como dependencia
 
     return (
-        <View style={styles.container}>
+        <ScrollView style={styles.container}>
             <View style={styles.card}>
                 <View style={styles.iconContainer}>
                     <IconButton icon='hamburger' size={90} disabled />
@@ -69,8 +122,41 @@ const PlaceDetailComponent = ({ route, navigation }) => {
                 <Text
                     style={styles.text}
                 >{`Número de reseñas: ${place.reviewCount}`}</Text>
+                <View style={styles.commentsCard}>
+                    <View>
+                        <Text style={styles.commentsTitle}>
+                            Añade algun comentario!
+                        </Text>
+                        {comments.map((comment, index) => (
+                            <View key={index} style={styles.commentContainer}>
+                                <View style={styles.commentTextContainer}>
+                                    <Text style={styles.commentText}>
+                                        {comment}
+                                    </Text>
+                                </View>
+                                <IconButton
+                                    icon='delete'
+                                    size={25}
+                                    iconColor='red'
+                                    onPress={() => handleDeleteComment(index)}
+                                />
+                            </View>
+                        ))}
+                    </View>
+
+                    {/* Input para agregar comentarios */}
+                    <View style={styles.commentInputContainer}>
+                        <TextInput
+                            style={styles.commentInput}
+                            placeholder='Añade un comentario...'
+                            value={newComment}
+                            onChangeText={handleCommentChange}
+                        />
+                        <Button title='Agregar' onPress={handleAddComment} />
+                    </View>
+                </View>
             </View>
-        </View>
+        </ScrollView>
     );
 };
 
@@ -108,6 +194,47 @@ const styles = StyleSheet.create({
     },
     boldText: {
         fontWeight: 'bold',
+    },
+    commentsTitle: {
+        fontSize: 18,
+        fontWeight: 'bold',
+        marginBottom: 10,
+        textAlign: 'center',
+    },
+    commentsCard: {
+        marginTop: 20,
+        padding: 10,
+        flex: 1,
+        borderRadius: 8,
+        backgroundColor: '#f5f5f5',
+        alignSelf: 'stretch',
+    },
+    commentText: {
+        fontSize: 16,
+        marginBottom: 5,
+        marginStart: 10,
+    },
+    commentInputContainer: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        marginTop: 10,
+    },
+    commentInput: {
+        flex: 1,
+        height: 40,
+        borderColor: 'gray',
+        borderWidth: 1,
+        borderRadius: 5,
+        marginRight: 10,
+        padding: 5,
+    },
+    commentTextContainer: {
+        flex: 1,
+    },
+    commentContainer: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        marginBottom: 5,
     },
 });
 
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,5 +1,5 @@
 export const ENV = {
-    API_URL: 'http://192.168.100.13:1337/api',
+    API_URL: 'http://192.168.100.21:1337/api',
     API_URL_RM: 'https://rickandmortyapi.com/api/character',
     APIKEY_PLACE: 'AIzaSyAxT9y1e41YVMLKKluEKxjS2ctQ1PzlAEw',
     YOUR_GOOGLE_PLACES_API_KEY: 'AIzaSyAxT9y1e41YVMLKKluEKxjS2ctQ1PzlAEw',
